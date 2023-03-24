17efe087fa18c4d8af2cd2234cb5e010022365c3
Humberto <hdkaratechop@gmail.com>
2023-03-23 19:19:00 -0700

    add method for cleaning raw html

--- san_joaquin_birds.rb
+++ san_joaquin_birds.rb
@@ -21,15 +21,22 @@ require 'nokogiri'
         doc = Nokogiri::HTML.parse(URI.open('https://www.sanjoaquinaudubon.org/field-checklist-of-the-birds-of-san-joaquin-county/'))
 
             bird_data= doc.css("div.elementor-toggle-item").map do |x|
-                species = x.at_css("div.elementor-tab-title a").text.strip 
+                species = x.at_css("div.elementor-tab-title a").text.strip
                 birds = x.at_css("div.elementor-tab-content.elementor-clearfix p").text.strip
 
                 Bird.new species, birds 
             end
 
+            def clean_bird_data(raw_html)
+                html = raw_html.encode('UTF-8', invalid: :replace, undef: :replace, replace: '', universal_newline: true).gsub(/\P{ASCII}/, '')
+                parser = Nokogiri::HTML(html, nil, Encoding::UTF_8.to_s)
+                parser.xpath('//script')&.remove
+                parser.xpath('//style')&.remove
+                parser.xpath('//text()').map(&:text).join(' ').squish
+              end
     
 
-pp bird_data
+pp clean_bird_data(bird_data)
 
 
 

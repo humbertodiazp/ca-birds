69d1d865c0bb6c825e4a1403caf94e9cdc66b774
Humberto <hdkaratechop@gmail.com>
2023-03-28 23:59:56 -0700

    Add hash for  bird species

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+SPECIES_AND_BIRDS = {
+    :LOONS => { "Red-throated Loon" },
+    :GREBES => { "Pied-billed Grebe" },
+    :SWANS_GEESE_AND_DUCKS => { "Fulvous Whistling-Duck, Tundra Swan, Whooper Swan, Trumpeter Swan, Greater White-fronted Goose, 
+        Snow Goose, Ross’ Goose, Brant, Canada Goose, Wood Duck, Green-winged Teal, Mallard, Northern Pintail, Blue-winged Teal, 
+        Cinnamon Teal, Northern Shoveler, Gadwall, Eurasian Wigeon, American Wigeon, Canvasback, Redhead, Ring-necked Duck, 
+        Tufted Duck, Greater Scaup, Lesser Scaup, Harlequin Duck, Oldsquaw, Surf Scoter, White-winged Scoter, Common Goldeneye, 
+        Barrow’s Goldeneye, Bufflehead, Hooded Merganser, Common Merganser, Red-breasted Merganser, Ruddy Duck"},
+   }.freeze

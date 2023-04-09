3fc6bf37bbc83e0b995d28be82c0c059074ddd9e
Humberto <hdkaratechop@gmail.com>
2023-04-09 14:51:19 -0700

    add script to generate an array of bird names

--- san_joaquin_birds.rb
+++ san_joaquin_birds.rb
@@ -20,23 +20,37 @@ require 'nokogiri'
         
         doc = Nokogiri::HTML.parse(URI.open('https://www.sanjoaquinaudubon.org/field-checklist-of-the-birds-of-san-joaquin-county/'))
 
-            bird_data= doc.css("div.elementor-toggle-item").map do |x|
-                species = x.at_css("div.elementor-tab-title a").text.strip
-                birds = x.at_css("div.elementor-tab-content.elementor-clearfix p").text.strip
+            bird_data= doc.css("div elementor-toggle-item").map do |x|
+                species = x.at_css("elementor-tab-title span a").text.strip
+                birds = x.at_css("div p").text.strip
 
                 Bird.new species, birds 
             end
 
-            def clean_bird_data(raw_html)
-                html = raw_html.encode('UTF-8', invalid: :replace, undef: :replace, replace: '', universal_newline: true).gsub(/\P{ASCII}/, '')
-                parser = Nokogiri::HTML(html, nil, Encoding::UTF_8.to_s)
-                parser.xpath('//script')&.remove
-                parser.xpath('//style')&.remove
-                parser.xpath('//text()').map(&:text).join(' ').squish
-              end
-    
+#             def clean_bird_data(raw_html)
+#                 html = raw_html.encode('UTF-8', invalid: :replace, undef: :replace, replace: '', universal_newline: true).gsub(/\P{ASCII}/, '')
+#                 parser = Nokogiri::HTML(html, nil, Encoding::UTF_8.to_s)
+#                 parser.xpath('//script')&.remove
+#                 parser.xpath('//style')&.remove
+#                 parser.xpath('//text()').map(&:text).join(' ').squish
+#               end
 
-pp clean_bird_data(bird_data)
+# after using .gsub to semi-manually clean the raw html generated above, 
+# use the below code to add quotes around every bird name to use in creating a dictionary of species and birds
+    
+# Split the string into an array of words
+birds = str.split(/\s*,\s*/)
+# Add quotation marks around each word
+    quoted_birds = birds.map do |bird|
+         if bird.include?(" ")
+         "\"#{word}\""
+        else
+            word
+        end
+    end
+
+puts quoted_birds.join(", ")
+
 
 
 

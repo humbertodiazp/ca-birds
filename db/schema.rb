804dccb0e77ed698df19ab3e3fb45656b0a3f4f7
Humberto <hdkaratechop@gmail.com>
2023-04-27 23:20:45 -0700

    run migrations for bird members table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_28_061015) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_28_061516) do
+  create_table "bird_members", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image_url"
+    t.integer "bird_type_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bird_type_id"], name: "index_bird_members_on_bird_type_id"
+  end
+
   create_table "bird_types", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bird_members", "bird_types"
 end
